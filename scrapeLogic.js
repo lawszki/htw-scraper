d24bb4918dceca697f1479e4be469aa4425dd732
lawszki <d.ronski@icloud.com>
2024-06-22 13:30:32 +0200

    Updated args and executable path for launch

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+require("dotenv").config();
 
 const scrapeLogic = async (req, res) => {
     function cleanText(text) {
@@ -8,7 +9,17 @@ const scrapeLogic = async (req, res) => {
 
     let browser;
     try {
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({
+            args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote"
+            ],
+            executablePath: process.env.NODE_ENV === "production"
+                ?  process.env.PUPPETEER_EXECUTABLE_PATH
+                : puppeteer.executablePath(),
+        });
         const page = await browser.newPage();
         // Seite aufrufen
         await page.goto('https://sport.htw-berlin.de/angebote/aktueller_zeitraum/index.html');
